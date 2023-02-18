1b4ca170181cede6f7f0a78480b3e278a892517c
Abdul Ahad <100588393+Abdul-WD@users.noreply.github.com>
2023-02-18 10:11:10 +0500

    homworks lesson11

---
+++ lesson-11-for.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 14:38:45 2023
+
+@author: WINDOWS-11
+"""
+
+# users = ['Abror', 'Mansur', 'Kozim', 'Vali']
+# for user in users:
+#     print('Welcome,', user)
+
+# numbers = list(range(1, 11))
+# for n in numbers:
+#     print(f"{n} ning kvadrati {n**2} ga teng")
+
+# sonlar = list(range(1,20,2))
+# sonlar_kvadrati = []
+# for n in sonlar:
+#     sonlar_kvadrati.append(n**2)
+    
+# print(sonlar)
+# print(sonlar_kvadrati)
+
+# students = []
+# for n in range(11):
+#     students.append(input(f"{n+1}-talaba:>>>").capitalize())
+
+# print(students)
+
+# cars = ['audi', 'bmw', 'hyundai', 'toyoto', 'kia']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+        
+
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# ovqat = input('Nima ovqat yeysiz?>>>')
+# if ovqat.lower() in menu:
+#     print('Buyurtma qabul qilindi.')
+# else:
+#     print('Afsuski bizda bunday ovqat yo\'q')  
+
+# son = int(input("Juft son kiriting>>>"))
+# if son%2 == 0:
+#     print('Rahmat')
+# else:
+#     print('Bu juft son emas!')
+        
+
+# yosh = int(input("Yoshingiz? >>> "))
+
+# if yosh < 4 or yosh > 60:
+#     narh = 0
+# elif yosh < 18:
+#     narh = 10000
+# else:
+#     narh = 20000
+# print(f'Sizga muzeyga kirish {narh} so`m')
+
+son1 = float(input('Birinchi sonni kiriting: >> '))
+son2 = float(input('Ikkinchi sonni kiriting: >> '))
+
+if son1 > son2:
+    print(f'{son1} > {son2}')
+elif son1 == son2:
+    print(f'{son1} = {son2}')
+else:
+    print(f'{son1} < {son2}')
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
