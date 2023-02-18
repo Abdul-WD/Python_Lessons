0b21d89dde08e4068e19c3c59c1261fad3c510bc
Abdul Ahad <100588393+Abdul-WD@users.noreply.github.com>
2023-02-18 10:08:26 +0500

    Create 1-dars.py

---
+++ 1-dars.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 12:25:44 2023
+
+@author: WINDOWS-11
+"""
+
+
+kocha = input("Ko`changiz nomi?\n")
+mahalla = input("Mahallangiz nomi?\n")
+tuman = input("Tumaningiz nomi?\n")
+viloyat = input("Qaysi viloyat?\n")
+adres = f"{kocha.title()} ko`chasi, {mahalla.upper()} mahallasi, {tuman.title()} tumani, {viloyat.upper()} viloyati "
+print(adres)
