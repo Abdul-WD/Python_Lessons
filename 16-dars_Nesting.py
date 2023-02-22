9d663ec77a1e62204c03db4b6342a06d0d5e55f6
Abdul Ahad <100588393+Abdul-WD@users.noreply.github.com>
2023-02-22 15:47:46 +0500

    Create 16-dars_Nesting.py

---
+++ 16-dars_Nesting.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 12:04:11 2023
+
+@author: WINDOWS-11
+"""
+
+
+# malibus = []
+
+# for n in range(10):
+#     new_car = {
+#         'model': 'malibu',
+#         'rangi': None,
+#         'yil' : 2022,
+#         'narxi' : None,
+#         'km' : 0,
+#         'karobka' : 'avto'
+#         }
+#     malibus.append(new_car)
+
+# for malibu in malibus[:3]:
+#     malibu['rangi'] = 'oq'
+    
+# for malibu in malibus[3:6]:
+#     malibu['rangi'] = 'qora'
+    
+# for malibu in malibus[6:]:
+#     malibu['rangi'] = 'qora'
+#     malibu['karobka'] = 'mexanika'
+    
+# for malibu in malibus:
+#     if malibu['karobka'] == 'avto':
+#         malibu['narxi'] = 45000
+#     else:
+#         malibu['narxi'] = 40000
+    
+# for malibu in malibus:
+#     print(malibu)
+
+# developers = {
+#     'ali' : ['python', 'c++'],
+#     'vali' : ['html', 'css', 'js'],
+#     'hasan' : ['php', 'sql'],
+#     'maryam' : ['c++', 'c#'],
+#     'husan' : ['python', 'php']
+#     }
+
+# for developer, language in developers.items():
+#     print(f"\n{developer.title()} quyidagi dasturlash tillarini biladi: ", end='')
+#     for til in language:
+#         print(f"{til.upper() }", end=' ')
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
